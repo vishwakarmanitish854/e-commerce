361e0668ab902bc2673dc7a3c5fd7862b86d116e
Vikas Vishwakarma <welsomeit.web@gmail.com>
2024-08-23 12:14:14 +0530

    new button style done

--- src/app/usershop/item-card/item-card.component.ts
+++ src/app/usershop/item-card/item-card.component.ts
@@ -6,11 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-card.component.css']
 })
 export class ItemCardComponent implements OnInit {
-productById: any;
+  productById: any;
+  added: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const button = document.querySelector(".addtocart") as HTMLButtonElement | null;
+    const done = document.querySelector(".done") as HTMLElement | null;
+    console.log(button);
 
+    if (button && done) {
+      button.addEventListener('click', () => {
+        if (this.added) {
+          done.style.transform = "translate(-110%) skew(-40deg)";
+          this.added = false;
+        } else {
+          done.style.transform = "translate(0px)";
+          this.added = true;
+        }
+      });
+    }
+  }
 }
