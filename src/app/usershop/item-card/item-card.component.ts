49605afc51d92548f988db681a836511df49f2cd
Vikas Vishwakarma <welsomeit.web@gmail.com>
2024-08-23 12:00:47 +0530

    new card added

---
+++ src/app/usershop/item-card/item-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-card',
+  templateUrl: './item-card.component.html',
+  styleUrls: ['./item-card.component.css']
+})
+export class ItemCardComponent implements OnInit {
+productById: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
